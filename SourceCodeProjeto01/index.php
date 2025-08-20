aaa388a45a663d6af055bfe05f587698b3ab561e
GuaxinimEmo <geancrfreitas@gmail.com>
2025-08-20 12:22:56 -0300

    a

--- SourceCodeProjeto01/index.php
+++ SourceCodeProjeto01/index.php
@@ -8,13 +8,24 @@
     
 </head>
 <body>
+    <?php
+    session_start();
+    if (isset($_SESSION['user'])) {
+        echo "<h1>Bem-vindo, " . $_SESSION['user'] . "!</h1><br>";
+    } else {
+        echo "<h1>Sem User conectado</h1><br>";
+    }
+    ?>
+    <br>
+    <div id="menu">
+            <h2>Menu Principal</h2>
+        <ul>
+            <li><a href="cadastro.html"><button>Cadastro</button></a></li>
+            <li ><a href="listar.php"><button>Listar Cadastrados</button></a></li>
+            <li><a href="login.html"><button>Login</button></a></li>
+            <li><a href="logout.php"><button>Logout</button></a></li>
+        </ul>
+    </div>
 
-    <h2>Menu Principal</h2>
-    <ul>
-        <li><a href="cadastro.html">Cadastro</a></li>
-        <li ><a href="listar.php">Listar Cadastrados</a></li>
-        <li><a href="login.html">Login</a></li>
-        <li><a href="logout.php">Logout</a></li>
-    </ul>
 </body>
 </html>
--- SourceCodeProjeto01/logout.php
+++ SourceCodeProjeto01/logout.php
@@ -4,8 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
-    
+    <?php
+    session_start();
+    session_destroy(); // Destroi a sessão
+    header("Location: index.php");
+    ?>
 </body>
 </html>
